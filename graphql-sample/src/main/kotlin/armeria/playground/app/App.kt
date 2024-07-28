03c9ffa714abec5ea67d03ddff24686dab52373d
Doğaç Eldenk <dogacel@gmail.com>
2024-07-28 12:46:56 -0500

    checkpoint

--- graphql-sample/src/main/kotlin/armeria/playground/app/App.kt
+++ graphql-sample/src/main/kotlin/armeria/playground/app/App.kt
@@ -10,6 +10,14 @@ import graphql.schema.idl.RuntimeWiring
 import graphql.schema.idl.SchemaGenerator
 import graphql.schema.idl.SchemaParser
 import graphql.schema.idl.TypeRuntimeWiring
+import java.util.concurrent.CompletionStage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.future.future
+import org.dataloader.BatchLoader
+import org.dataloader.DataLoader
+import org.dataloader.DataLoaderFactory
 import org.dataloader.DataLoaderRegistry
 
 suspend fun main() {
@@ -36,7 +44,8 @@ data class Group(
 
 object PrimaryGraphqlService {
     fun new(): GraphqlService {
-        val schema = javaClass.getResource("/schema.graphqls")?.readText()!!
+        val schema =
+            javaClass.getResource("/schema.graphqls")?.readText() ?: error("resources/schema.graphqls not found")
 
         // Parse schema
         val schemaParser = SchemaParser()
@@ -60,6 +69,8 @@ object PrimaryGraphqlService {
         val dataLoaderRegistry =
             DataLoaderRegistry
                 .newRegistry()
+                .register("user", UserBatchLoader().asDataLoader())
+                .register("group", GroupBatchLoader().asDataLoader())
                 .build()
 
         // Generate schema
@@ -81,30 +92,66 @@ val users: Map<String, User> =
         "2" to User("2", "human"),
         "3" to User("3", "droid"),
     )
-
-class UserDataFetcher : DataFetcher<User?> {
-    override fun get(environment: DataFetchingEnvironment): User? {
-        val id: String = environment.getArgument("id")
-        return users[id]
-    }
-}
-
 val groups: Map<String, Group> =
     mapOf(
         "1" to Group("1", "admin", listOf("1", "2")),
         "2" to Group("2", "user", listOf("3")),
     )
 
-class GroupDataFetcher : DataFetcher<Group?> {
-    override fun get(environment: DataFetchingEnvironment): Group? {
+
+class UserDataFetcher : DataFetcher<CompletionStage<User?>> {
+    override fun get(environment: DataFetchingEnvironment): CompletionStage<User?> {
         val id: String = environment.getArgument("id")
-        return groups[id]
+
+        val userDataLoader = environment.getDataLoader<String, User>("user")
+
+        return userDataLoader.load(id)
     }
 }
 
-class GroupUserDataFetcher : DataFetcher<List<User>> {
-    override fun get(environment: DataFetchingEnvironment): List<User> {
+
+class GroupDataFetcher : DataFetcher<CompletionStage<Group?>> {
+    override fun get(environment: DataFetchingEnvironment): CompletionStage<Group?> {
+        val id: String = environment.getArgument("id")
+
+        val groupDataLoader = environment.getDataLoader<String, Group>("group")
+
+        return groupDataLoader.load(id)
+    }
+}
+
+class GroupUserDataFetcher : DataFetcher<CompletionStage<List<User>>> {
+    override fun get(environment: DataFetchingEnvironment): CompletionStage<List<User>> {
         val group: Group = environment.getSource()
-        return group.userIds.mapNotNull { users[it] }
+
+        val userDataLoader = environment.getDataLoader<String, User>("user")
+
+        return userDataLoader.loadMany(group.userIds)
+    }
+}
+
+class UserBatchLoader : BatchLoader<String, User> {
+    override fun load(keys: List<String>): CompletionStage<List<User>> {
+        return CoroutineScope(Dispatchers.IO).future {
+            delay(250)
+            keys.mapNotNull { users[it] }
+        }
+    }
+
+    fun asDataLoader(): DataLoader<String, User> {
+        return DataLoaderFactory.newDataLoader(this)
+    }
+}
+
+class GroupBatchLoader : BatchLoader<String, Group> {
+    override fun load(keys: List<String>): CompletionStage<List<Group>> {
+        return CoroutineScope(Dispatchers.IO).future {
+            delay(250)
+            keys.mapNotNull { groups[it] }
+        }
+    }
+
+    fun asDataLoader(): DataLoader<String, Group> {
+        return DataLoaderFactory.newDataLoader(this)
     }
 }
